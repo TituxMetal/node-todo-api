3db30d0198086bc228e7ce28021768e5419ed8e0
Laptop | Lang Guillaume <lang.guillaume@gmail.com>
2018-03-07 05:21:04 +0100

    Add GET /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const {ObjectID} = require('mongodb')
 
 const {mongoose} = require('./db/mongoose')
 const {Todo} = require('./models/todo')
@@ -20,10 +21,29 @@ app.post('/todos', (req, res) => {
 	})
 })
 
+// Get all todos
 app.get('/todos', (req, res) => {
 	Todo.find().then((todos) => {
 		res.status(200).send({ todos })
 	}).catch((err) => res.status(400).send(err))
+})
+
+// Get a single todos by its ID
+app.get('/todos/:id', (req, res) => {
+	const todoId = req.params.id
+
+	if (!ObjectID.isValid(todoId)) {
+		return res.status(404).send()
+	}
+
+	Todo.findById(todoId).then((todo) => {
+		if (!todo) {
+			return res.status(404).send()
+		}
+
+		res.send({ todo })
+
+	}).catch((err) => res.status(400).send())
 })
 
 app.listen(3000, () => {
