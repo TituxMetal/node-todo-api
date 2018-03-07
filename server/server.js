aacbe1e084fda4faaa97442b8eac5fa37ebdb74a
Laptop | Lang Guillaume <lang.guillaume@gmail.com>
2018-03-07 22:45:01 +0100

    Add PATCH /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash')
 const express = require('express')
 const bodyParser = require('body-parser')
 const {ObjectID} = require('mongodb')
@@ -45,6 +46,31 @@ app.get('/todos/:id', (req, res) => {
 		res.send({ todo })
 
 	}).catch((err) => res.status(400).send())
+})
+
+// Partial update a single todo by its ID
+app.patch('/todos/:id', (req, res) => {
+	const id = req.params.id
+	let body = _.pick(req.body, ['text', 'completed'])
+
+	if (!ObjectID.isValid(id)) {
+		return res.status(404).send()
+	}
+
+	if (_.isBoolean(body.completed) && body.completed) {
+		body.completedAt = new Date().getTime()
+	} else {
+		body.completed = false
+		body.completedAt = null
+	}
+
+	Todo.findByIdAndUpdate(id, { $set: body }, { new: true }).then((todo) => {
+		if (!todo) {
+			return res.status(404).send()
+		}
+
+		res.send({ todo })
+	}).catch((e) => res.status(400).send())
 })
 
 // Delete a todo by its ID
