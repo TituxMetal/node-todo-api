9a2ea4688b007aeec57b7a0552a2513f176fe5b9
Laptop | Lang Guillaume <lang.guillaume@gmail.com>
2018-03-10 15:35:31 +0100

    Hash user password before saving it

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,13 +1,28 @@
 const {SHA256} = require('crypto-js')
 const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
 
-const data = { id: 10 }
+let password = '123aze!'
 
-const token = jwt.sign(data, '123aze')
-console.log(token)
+// bcrypt.genSalt(11, (err, salt) => {
+// 	bcrypt.hash(password, salt, (err, hash) => {
+// 		console.log(hash)
+// 	})
+// })
 
-const decoded = jwt.verify(token, '123aze')
-console.log(decoded)
+const hashedPassword = '$2a$11$ATtkZmWhGJJX66XGHNY.LeMHataIUev39Dk1xIFpHTXt6x/b0SBXK'
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+	console.log(res)
+})
+
+// const data = { id: 10 }
+//
+// const token = jwt.sign(data, '123aze')
+// console.log(token)
+//
+// const decoded = jwt.verify(token, '123aze')
+// console.log(decoded)
 
 // const message = 'I am user number 3'
 // const hash = SHA256(message).toString()
