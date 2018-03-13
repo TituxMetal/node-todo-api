53ce825db21bcfb7b2da1c903c9e037f0fb10a99
Laptop | Lang Guillaume <lang.guillaume@gmail.com>
2018-03-13 14:25:04 +0100

    Add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -52,6 +52,26 @@ UserSchema.methods.generateAuthToken = function () {
 	}).catch((err) => console.log(err))
 }
 
+UserSchema.statics.findByCredentials = function (email, password) {
+	const User = this
+
+	return User.findOne({ email }).then((user) => {
+		if (!user) {
+			return Promise.reject()
+		}
+
+		return new Promise((resolve, reject) => {
+			bcrypt.compare(password, user.password, (err, res) => {
+				if (!res) {
+					return reject()
+				}
+
+				return resolve(user)
+			})
+		})
+	})
+}
+
 UserSchema.statics.findByToken = function (token) {
 	const User = this
 	let decoded
--- server/server.js
+++ server/server.js
@@ -105,6 +105,18 @@ app.post('/users', (req, res) => {
 	}).catch((err) => res.status(400).send(err))
 })
 
+app.post('/users/login', (req, res) => {
+	let body = _.pick(req.body, ['email', 'password'])
+
+	User.findByCredentials(body.email, body.password).then((user) => {
+		return user.generateAuthToken().then((token) => {
+			res.header('x-auth', token).send(user)
+		})
+	}).catch((err) => {
+		res.status(400).send(err)
+	})
+})
+
 app.get('/users/me', authenticate, (req, res) => {
 	res.send(req.user)
 })
