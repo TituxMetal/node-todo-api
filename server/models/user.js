8bb057a77ccc128cabb2974a2fd4f84c3252126d
Laptop | Lang Guillaume <lang.guillaume@gmail.com>
2018-03-16 14:19:45 +0100

    Making todos routes private

--- server/models/user.js
+++ server/models/user.js
@@ -43,7 +43,7 @@ UserSchema.methods.toJSON = function () {
 UserSchema.methods.generateAuthToken = function () {
 	const user = this
 	const access = 'auth'
-	const token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString()
+	const token = jwt.sign({_id: user._id.toHexString(), access}, process.env.JWT_SALT).toString()
 
 	user.tokens = user.tokens.concat([{ access, token }])
 
@@ -87,7 +87,7 @@ UserSchema.statics.findByToken = function (token) {
 	let decoded
 
 	try {
-		decoded = jwt.verify(token, 'abc123')
+		decoded = jwt.verify(token, process.env.JWT_SALT)
 	} catch (e) {
 		return Promise.reject()
 	}
--- server/server.js
+++ server/server.js
@@ -16,8 +16,11 @@ const port = process.env.PORT || 3000
 app.use(bodyParser.json())
 
 // Create a todo
-app.post('/todos', (req, res) => {
-	const todo = new Todo({ text: req.body.text })
+app.post('/todos', authenticate, (req, res) => {
+	const todo = new Todo({
+		text: req.body.text,
+		_creator: req.user._id
+	})
 
 	todo.save().then((todo) => {
 		res.send({ todo })
@@ -27,21 +30,21 @@ app.post('/todos', (req, res) => {
 })
 
 // Get all todos
-app.get('/todos', (req, res) => {
-	Todo.find().then((todos) => {
+app.get('/todos', authenticate, (req, res) => {
+	Todo.find({ _creator: req.user._id }).then((todos) => {
 		res.status(200).send({ todos })
 	}).catch((err) => res.status(400).send(err))
 })
 
 // Get a single todos by its ID
-app.get('/todos/:id', (req, res) => {
+app.get('/todos/:id', authenticate, (req, res) => {
 	const todoId = req.params.id
 
 	if (!ObjectID.isValid(todoId)) {
 		return res.status(404).send()
 	}
 
-	Todo.findById(todoId).then((todo) => {
+	Todo.findOne({ _id: todoId, _creator: req.user._id }).then((todo) => {
 		if (!todo) {
 			return res.status(404).send()
 		}
@@ -52,7 +55,7 @@ app.get('/todos/:id', (req, res) => {
 })
 
 // Partial update a single todo by its ID
-app.patch('/todos/:id', (req, res) => {
+app.patch('/todos/:id', authenticate, (req, res) => {
 	const id = req.params.id
 	let body = _.pick(req.body, ['text', 'completed'])
 
@@ -67,7 +70,7 @@ app.patch('/todos/:id', (req, res) => {
 		body.completedAt = null
 	}
 
-	Todo.findByIdAndUpdate(id, { $set: body }, { new: true }).then((todo) => {
+	Todo.findOneAndUpdate({ _id: id, _creator: req.user._id }, { $set: body }, { new: true }).then((todo) => {
 		if (!todo) {
 			return res.status(404).send()
 		}
@@ -77,14 +80,14 @@ app.patch('/todos/:id', (req, res) => {
 })
 
 // Delete a todo by its ID
-app.delete('/todos/:id', (req, res) => {
+app.delete('/todos/:id', authenticate, (req, res) => {
 	const todoId = req.params.id
 
 	if (!ObjectID.isValid(todoId)) {
 		return res.status(404).send()
 	}
 
-	Todo.findByIdAndRemove(todoId).then((todo) => {
+	Todo.findOneAndRemove({ _id: todoId, _creator: req.user._id }).then((todo) => {
 		if (!todo) {
 			return res.status(404).send()
 		}
@@ -94,6 +97,7 @@ app.delete('/todos/:id', (req, res) => {
 	}).catch((err) => res.status(404).send())
 })
 
+// Signup a new user
 app.post('/users', (req, res) => {
 	let body = _.pick(req.body, ['email', 'password'])
 	let user = new User(body)
@@ -105,6 +109,7 @@ app.post('/users', (req, res) => {
 	}).catch((err) => res.status(400).send(err))
 })
 
+// Login a user
 app.post('/users/login', (req, res) => {
 	let body = _.pick(req.body, ['email', 'password'])
 
@@ -115,10 +120,12 @@ app.post('/users/login', (req, res) => {
 	}).catch((err) => res.status(400).send(err))
 })
 
+// Get the current authenticated user
 app.get('/users/me', authenticate, (req, res) => {
 	res.send(req.user)
 })
 
+// Logout the current authenticated user
 app.delete('/users/me/token', authenticate, (req, res) => {
 	req.user.removeToken(req.token).then(() => {
 		res.status(200).send()
